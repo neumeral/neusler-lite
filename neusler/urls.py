ee0c80cd183a018cf29920205f76564e1cc43922
Awin Abi <awin@awinawin.awin>
2022-11-07 19:53:11 +0530

    change api and other urls

--- neusler/urls.py
+++ neusler/urls.py
@@ -8,19 +8,15 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from neusler.search import views as search_views
-from neusler.search.views import SearchAPI
-
-from .api import api_router
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("comments/", include("django_comments_xtd.urls")),
-    path("cms/api/", include("neusler.cms.api.urls")),
-    path("accounts/api/", include("neusler.accounts.api.urls")),
     path("rss/", include("neusler.cms.feed.urls")),
     path("front/", include("neusler.front.urls")),
+    path("api/v2/", include("neusler.api")),
 ]
 
 if settings.DEBUG:
@@ -34,10 +30,7 @@ if settings.DEBUG:
 
 urlpatterns = urlpatterns + i18n_patterns(
     path("search/", search_views.search, name="search"),
-    path("api/search/", SearchAPI.as_view(), name="search_api"),
     path("accounts/", include("neusler.accounts.urls")),
-    path("accounts/", include("allauth.urls")),
-    path("api/v2/", api_router.urls),
     path("", include(wagtail_urls)),
     prefix_default_language=False,
 )
